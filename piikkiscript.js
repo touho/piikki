f3d9cd158a936040e618054cc9b874daf71ea210
AnnaMaritta <anna.m.torronen@gmail.com>
2015-03-23 20:09:19 +0200

    Alasvetovalikon tyylittelyä

--- piikkiscript.js
+++ piikkiscript.js
@@ -410,7 +410,7 @@ function PiikkiUtil()
 
 			auto.innerHTML = code;
 			if (visible != auto.style.visibility) auto.style.visibility = visible;
-			auto.style.display = "block";
+			auto.style.display = "inline";
 		}
 	}
 
